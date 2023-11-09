20beaa84fbd6f69ec44da34ac449eb7de5421b41
Glex <Alex20004044@yandex.ru>
2023-11-09 22:06:11 +0300

    Init

---
+++ Core.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ISA_2
+{
+
+    public class Core
+    {
+        VideoCapture videoCapture = new VideoCapture();
+        CascadeClassifier defaultClassifier = new CascadeClassifier(@"D:\Data\Projects\ISA 2.0\ISA 2\haarcascade_frontalface_default.xml");
+        
+        float faceWidthToDistanceCoefficient = 12.317f;
+
+        int frameWidth = 320;
+        int frameHeight = 240;
+
+        float minDistance = 40;
+
+        int updateDelay = 200;
+        public Core()
+        {
+            videoCapture.Set(Emgu.CV.CvEnum.CapProp.FrameWidth, frameWidth);
+            videoCapture.Set(Emgu.CV.CvEnum.CapProp.FrameHeight, frameHeight);
+        }
+
+        ~Core()
+        {
+            videoCapture?.Dispose();
+        }
+
+        public void Start()
+        {
+            videoCapture.Start();
+            CvInvoke.NamedWindow("ISA");
+            while (true)
+            {
+                Mat mat = videoCapture.QueryFrame();
+
+                var outputMat = FindFaces(mat, out int faceWidth);
+                float distance = faceWidthToDistanceCoefficient / (float)(faceWidth) * frameWidth;
+
+                CvInvoke.Imshow("ISA", outputMat);
+                int key = CvInvoke.WaitKey(updateDelay);
+                if (key == (int)ConsoleKey.Escape)
+                    break;
+
+
+                Console.Write($"Distance: {distance.ToString("#")} ");
+
+                if (distance < minDistance)
+                    Console.WriteLine("Too close!");
+                else
+                    Console.WriteLine("Ok");
+            }
+        }
+
+        Mat FindFaces(Mat mat, out int faceWidth)
+        {
+            Image<Gray, byte> grayImage = mat.ToImage<Gray, byte>();
+            Rectangle[] facesDef = defaultClassifier.DetectMultiScale(grayImage, 1.1, 3);
+            int biggestFace = 0;
+            foreach (Rectangle x in facesDef)
+            {
+                CvInvoke.Rectangle(mat, x, new MCvScalar(0, 255, 0));
+
+                if (x.Width > biggestFace)
+                {
+                    biggestFace = x.Width;
+                }
+            }
+
+            faceWidth = biggestFace;
+            return mat;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Emgu.CV;
+
+namespace ISA_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core core = new Core();
+            core.Start();
+        }
+    }
+}
